780b61843f33d06cd3bd79e7aeebbc54cbb4b410
phucphuc123 <phuc1761995@gmail.com>
2019-09-26 15:05:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+/*
+Template name: Template Game mini
+Template Post Type: post, page
+
+*/
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>game mini</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style-game-mini.css?ver=<?php echo rand(1000,99999) ?>">
+</head>
+<body>
+
+
+<div class="wrap-game-mini-gift" style="display: none;">
+	<div class="game-mini-gift run">
+		<img src="assets/img/gift.png" alt="gift" title="Click">
+	</div>
+</div>
+
+<div class="div-wrap-game-mini-begin" style="display: nones;">
+	<img src="assets/img/start.jpg" alt="gift" title="Click">
+
+</div>
+
+<div class="div-wrap-game-mini" style="display: none;">
+
+
+
+
+	<div class="snow-container">
+	  <div class="snow foreground"></div>
+	  <div class="snow foreground layered"></div>
+	  <div class="snow middleground"></div>
+	  <div class="snow middleground layered"></div>
+	  <div class="snow background"></div>
+	  <div class="snow background layered"></div>
+	</div>
+
+
+
+<!-- 	<div class="game-bg-1">
+		<img src="assets/img/v.png">
+	</div>
+ -->
+
+ 	<div class="game-point-1">
+		Points: <span>0</span>
+	</div>
+
+	<div class="game-time-1">
+		
+	</div>
+
+
+
+	<div class="game-point-status-ok" style="display: none;">
+		+ 100 points
+	</div>
+
+	<div class="game-point-status-not" style="display: none;">
+		- 10 points
+	</div>
+
+
+	<div class="game-bg-2">
+		<img src="assets/img/v3.png">
+	</div>
+
+
+	<div class="game-mini-gift fi run">
+		<img src="assets/img/gift.png" alt="gift" title="Click">
+	</div>
+
+		
+	<div class="game-mini-mr">
+		<img src="assets/img/mr.png" alt="mr">
+	</div>
+
+
+<!-- 	<div class="game-mini-gift">
+		<img src="assets/img/gift.png" alt="gift" title="Click">
+	</div>
+
+
+	<div class="game-mini-gift">
+		<img src="assets/img/gift.png" alt="gift" title="Click">
+	</div>
+
+ -->
+
+	<div class="game-mini-gift fi run">
+		<img src="assets/img/gift.png" alt="gift" title="Click">
+	</div>
+
+	<!-- <div class="game-mini-house">
+		
+	</div> -->
+
+	<div class="game-mini-overlay" style="display: none;"></div>
+
+	<div class="game-mini-result" style="display: none;">
+
+		
+			<div class="game-mini-result-mess">
+				Congratulation !
+			</div>
+
+			<div class="game-mini-result-points">
+				
+			</div>
+
+			<div class="game-mini-result-begin btn-1">
+				Start game
+			</div>
+		
+
+
+	</div>
+
+
+</div>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="assets/js/style-game-mini.js?ver=<?php echo rand(1000,99999) ?>"></script>
+
+
+
+</body>
+</html>
+
+
+
+
+
